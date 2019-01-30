d79eed930defb2d04fbdbcfee42df4d4095ca478
Thomas Letsch <contact@thomas-letsch.de>
2019-01-30 20:38:00 +0100

    Remove redundant read from gateway

--- src/main/kotlin/Startup.kt
+++ src/main/kotlin/Startup.kt
@@ -7,11 +7,10 @@ class Startup {
         val discoveryData = future.join()
 
         val client = Client(discoveryData.sourceAddress, "000000000000", discoveryData.getGatewayId())
-        var readBytes = client.readBytes()
         // should read 5410EC03615000000000000600090153BA3FD391BAB9
         client.sendMessage(Package(Command.GET_NAME))
-        readBytes = client.readBytes()
-        client.sendMessage(Package(command = Command.LOGIN, payload = Payload.login("admin", "aundt")))
+        var readBytes = client.readBytes()
+        client.sendMessage(Package(command = Command.LOGIN, payload = Payload.login("thomas", "aaabbbccc")))
         readBytes = client.readBytes()
     }
 }
